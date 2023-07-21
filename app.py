a2abb689ee54e3fcb23935c4f6f7f2ff0e4285bb
Andrei3223 <a.gruzitski@gmail.com>
2023-07-21 17:33:57 +0300

    change design

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def process_main_page():
 
 
 def show_main_page():
-    # image = Image.open('data/titanic.jpg')
+    image = Image.open('Drive.jpg')
 
     st.set_page_config(
         layout="wide",
@@ -30,34 +30,51 @@ def show_main_page():
         # page_icon=image,
 
     )
-
-    st.write(
-        """
-        # Predict the price of used vehicle 
-        ### Enter data:
-        """
-    )
-    # st.image(image)
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.write(
+            """
+            # Predict the price of used vehicle 
+            ### Enter data:
+            """
+        )
+    with col2:
+        st.image(image, width=600)
 
 
 def get_dataframe():
     # reading data
-    year = st.number_input("The year of manufacture")
-    driven = st.number_input("km driven")
-    fuel = st.selectbox("Fuel type",
-                        ('Diesel', 'Petrol', 'CNG', 'LPG'))
-    seller = st.selectbox("Seller type", ('Individual', 'Dealer', 'Trustmark Dealer'))
-    transmission = st.selectbox('Transmission type'
-                                , ('Manual', 'Automatic'))
-    owner = st.selectbox('Owner type', ('First Owner', 'Second Owner',
-                                        'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'))
-    # mileage 	engine 	max_power 	seats
-    mileage = st.number_input("Mileage (in kmpl (km/kg) )")
-    engine = st.number_input("Engine volume (in CC)")
-    max_power = st.number_input('Max power (in bhp)')
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        year = st.number_input("The year of manufacture", 2019)
+    with col2:
+        driven = st.number_input("km driven", 35000)
+
+    col3, col4 = st.columns([1, 1])
+    with col3:
+        fuel = st.selectbox("Fuel type",
+                            ('Diesel', 'Petrol', 'CNG', 'LPG'))
+    with col4:
+        seller = st.selectbox("Seller type", ('Individual', 'Dealer', 'Trustmark Dealer'))
+
+    col5, col6 = st.columns([1, 1])
+    with col5:
+        transmission = st.selectbox('Transmission type'
+                                    , ('Manual', 'Automatic'))
+    with col6:
+        owner = st.selectbox('Owner type', ('First Owner', 'Second Owner',
+                                            'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'))
+
+    col7, col8, col9 = st.columns([1, 1, 1])
+    with col7:
+        mileage = st.number_input("Mileage (in kmpl (km/kg) )", 23)
+    with col8:
+        engine = st.number_input("Engine volume (in CC)", 1000)
+    with col9:
+        max_power = st.number_input('Max power (in bhp)', 50)
     seats = st.slider(
         "Seats number",
-        min_value=1, max_value=30, value=2, step=1)
+        min_value=1, max_value=30, value=5, step=1)
 
     # preparing data
     translatetion = {
@@ -83,6 +100,7 @@ def get_dataframe():
         "CNG": (lambda x: 1 if x == 'CNG' else 0)(fuel),
         "LPG": (lambda x: 1 if x == 'LPG' else 0)(fuel)
     }
+
     df = pd.DataFrame(data, index=[0])
 
     return df
@@ -91,9 +109,18 @@ def get_dataframe():
 def get_result(df: pd.DataFrame):
     if st.button("Show result"):
         model = load_model()
+        '''
+            ## Your car characteristics:
+        '''
         st.write(df)
+        '''
+            ## Approximate price of your car:
+        '''
         res = model.predict(df)
-        st.write(res)
+        # st.markdown('<p class="big-font">%$res%</p>', unsafe_allow_html=True)
+        st.write(f'{res[0]:0,.2f}', '$',
+                 unsafe_allow_html=True)
+        # st.write(res[0])
 
 
 if __name__ == "__main__":
